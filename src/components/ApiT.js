223ab590393141c6d7c5fdb284cd661424d388a1
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2022-06-21 22:13:42 +0300

    21.06.22_3

--- src/components/ApiT.js
+++ src/components/ApiT.js
@@ -1,11 +1,11 @@
-export default class ApiT{
-  constructor(config){
+export default class Api {
+  constructor(config) {
     this._baseUrl = config.baseUrl;
     this._headers = config.headers;
   }
 
   //функция проверки запросов
-  _checkResponseServer(answ){
+  _checkResponseServer(answ) {
     if (answ.ok) {
       return answ.json();
     }
@@ -13,11 +13,75 @@ export default class ApiT{
   }
 
 
-//получаем инфо о пользователе с сервера
-getUserData() {
-  return fetch(`${this._baseUrl}/users/me`, {
-    headers: this._headers,
-  }).then((answ) => checkResponseServer(answ));
-}
+  //получаем инфо о пользователе с сервера
+  getUserData() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then((answ) => checkResponseServer(answ));
+  }
 
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then((answ) => this._checkResponseServer(answ));
+  }
+
+  //отправляем данные пользователя на сервер
+  changeUserInfo(userData) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userData.name,
+        about: userData.about,
+      }),
+    }).then((answ) => this._checkResponseServer(answ));
+  }
+
+  //изменяем аватар
+  changeAvatar(url) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: url,
+      }),
+    }).then((answ) => this._checkResponseServer(answ));
+  }
+
+  //добавляем лайк карточке
+  setLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then((answ) => this._checkResponseServer(answ));
+  }
+
+  //удаляем лайк
+  deleteLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((answ) => this._checkResponseServer(answ));
+  }
+
+  //добавляем новую карточку
+  createNewCard(card) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        link: data.link,
+      }),
+    }).then((answ) => checkResponseServer(answ));
+  }
+
+  //удаляем карточку
+  deleteCard(id) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then((answ) => this._checkResponseServer(answ));
+  }
 }
